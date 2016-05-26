f98226e6951c34aa6af53f6b25936d63ec45e42b
surender-eidiko <surender.molugu@eidiko.com>
2016-05-26 15:06:34 +0530

    code update

---
+++ src/main/java/com/cisco/integrations/model/CommonBean.java
@@ -0,0 +1,29 @@
+package com.cisco.integrations.model;
+
+public class CommonBean {
+	
+	private String roomId;
+	private String displayName;
+	private String imagePath;
+	
+	public String getRoomId() {
+		return roomId;
+	}
+	public void setRoomId(String roomId) {
+		this.roomId = roomId;
+	}
+	public String getDisplayName() {
+		return displayName;
+	}
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
+	public String getImagePath() {
+		return imagePath;
+	}
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+	
+
+}
